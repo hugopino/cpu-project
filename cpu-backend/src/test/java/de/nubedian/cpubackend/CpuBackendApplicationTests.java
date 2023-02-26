a5b81c45b35b1ce5be20d93a8f5d764ca77fcf25
Hugo <dev@hugopino.com>
2023-02-26 21:16:48 +0100

    Backend initial commit

---
+++ cpu-backend/src/test/java/de/nubedian/cpubackend/CpuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nubedian.cpubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
